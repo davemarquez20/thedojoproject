12239c71298bb3b1ca7054effd3a256c5f2d85ec
davemarquez20 <52246960+davemarquez20@users.noreply.github.com>
2022-12-05 19:30:05 +0800

    initial commit from local machine

---
+++ src/pages/project/Project.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+
+// components
+import ProjectComments from "./ProjectComments";
+import ProjectSummary from "./ProjectSummary";
+
+// styles
+import "./Project.css";
+
+export default function Project() {
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+  console.log(document.category);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <ProjectSummary project={document} />
+      <ProjectComments project={document} />
+    </div>
+  );
+}
